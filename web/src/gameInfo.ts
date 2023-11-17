dff0616a374ca87d6aabe3c5bca5ba8f3dd70545
balduvian <emmettglaser@gmail.com>
2023-11-16 21:58:28 -1000

    expand the socket receiver

--- web/src/gameInfo.ts
+++ web/src/gameInfo.ts
@@ -1,79 +1,96 @@
 import { create } from 'zustand';
 import { combine } from 'zustand/middleware';
-import { original, produce } from 'immer';
+import { Immutable } from 'immer';
+import { Log } from './log.js';
 
-export type Color = [number, number, number];
+export type Color = Immutable<[number, number, number]>;
 
-export type Player = {
+export type Player = Immutable<{
 	id: number;
 	color: Color;
 	name: string;
-};
+	teamId: number | undefined;
+}>;
 
-export type Team = {
+export type CachedPlayer = Immutable<
+	Player & {
+		cacheTimestamp: number;
+	}
+>;
+
+export type Team = Immutable<{
 	id: number;
 	name: string;
-};
+}>;
 
-export type Board = {
+export type Board = Immutable<{
 	width: number;
 	height: number;
 	board: number[];
 	flags: number[];
 	revealed: boolean[];
-};
+}>;
 
-export type Cursor = {
+export type Cursor = Immutable<{
 	x: number;
 	y: number;
 	playerId: number;
-};
+}>;
 
-export type GameState = {
+export type PlayerGameStats = Immutable<{
+	alive: boolean;
+}>;
+
+export type GameState = Immutable<{
 	board: Board;
 	gameTimer: number;
 	isNoGuessing: boolean;
 	cursorUpdateRate: number;
 	cursors: Cursor[];
-};
+	playersGameState: { [id: number]: PlayerGameStats };
+}>;
 
-export type GlobalState = {
+export type GlobalState = Immutable<{
+	playerCache: CachedPlayer[];
 	players: Player[];
 	teams: Team[];
 	selfPlayerId: number | undefined;
 	game: GameState | undefined;
-};
+	log: Log.Item[];
+}>;
 
 const initialGlobalState: GlobalState = {
+	playerCache: [],
 	players: [],
 	teams: [],
 	selfPlayerId: undefined,
 	game: undefined,
+	log: [],
 };
 
-const useGameState = create(
+export const useGameState = create(
 	combine({ ...initialGlobalState }, set => ({
-		setSelfPlayerId: (selfPlayerId: number | undefined) =>
-			set(
-				produce((state: GlobalState) => {
-					state.selfPlayerId = selfPlayerId;
-				}),
-			),
-		setPlayers: (players: Player[]) =>
-			set(
-				produce((state: GlobalState) => {
-					state.players = players;
-				}),
-			),
-		updatePlayer: (player: Player) =>
-			set(
-				produce((state: GlobalState) => {
-					const list = original(state.players);
-					if (list === undefined) return;
-					const index = list.findIndex(({ id }) => id === player.id);
-
-					state.players[index] = player;
-				}),
-			),
+		//setSelfPlayerId: (selfPlayerId: number | undefined) =>
+		//	set(
+		//		produce((state: GlobalState) => {
+		//			state.selfPlayerId = selfPlayerId;
+		//		}),
+		//	),
+		//setPlayers: (players: Player[]) =>
+		//	set(
+		//		produce((state: GlobalState) => {
+		//			state.players = players;
+		//		}),
+		//	),
+		//updatePlayer: (player: Player) =>
+		//	set(
+		//		produce((state: GlobalState) => {
+		//			const list = original(state.players);
+		//			if (list === undefined) return;
+		//			const index = list.findIndex(({ id }) => id === player.id);
+		//
+		//			state.players[index] = player;
+		//		}),
+		//	),
 	})),
 );
---
+++ web/src/log.ts
@@ -0,0 +1,29 @@
+import { Color } from './gameInfo.js';
+
+export namespace Log {
+	export enum Type {
+		TEAM_NAME_UPDATE,
+		TEAM_REMOVE,
+	}
+
+	export type BaseItem = {
+		type: Type;
+	};
+
+	export type TeamNameUpdate = BaseItem & {
+		type: Type.TEAM_NAME_UPDATE;
+		byPlayerId: number;
+		teamId: number;
+		oldName: string;
+		newName: string;
+	};
+
+	export type TeamRemove = BaseItem & {
+		type: Type.TEAM_REMOVE;
+		byPlayerId: number;
+		teamId: number;
+		teamName: string;
+	};
+
+	export type Item = TeamNameUpdate | TeamRemove;
+}
