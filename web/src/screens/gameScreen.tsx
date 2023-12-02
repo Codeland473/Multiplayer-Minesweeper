0d6975b71760802e9dc5ecee7c7bc54e35b0647e
balduvian <emmettglaser@gmail.com>
2023-12-01 23:25:48 -1000

    timer

--- web/src/screens/gameScreen.tsx
+++ web/src/screens/gameScreen.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Game, TeamData, update, useGlobalState } from '../globalState.js';
 import { Board } from '../components/board.js';
-import { Sender } from '../socket/sender.js';
 import { handleClickTile, isShownTeamData } from '../tiles.js';
 
 export const GameScreen = () => {
 	const game = useGlobalState(state => state.game)!;
 	const selfPlayerId = useGlobalState(state => state.selfPlayerId);
 	const players = useGlobalState(state => state.players);
+
+	const [currentTime, setCurrentTime] = React.useState(Date.now());
+	const secondsTillStart = Math.ceil((game.startTime - currentTime) / 1000);
+	const isGameGoing = secondsTillStart <= 0;
 
 	const selfPlayer = players.find(({ id }) => id === selfPlayerId)!;
 	if (selfPlayer.teamId === undefined) throw Error('spec not impl');
@@ -22,6 +25,7 @@ export const GameScreen = () => {
 
 	const onClickBoard = React.useCallback(
 		(x: number, y: number, button: number) => {
+			if (!isGameGoing) return;
 			update(draftState => {
 				if (draftState.game === undefined) return;
 				if (selfPlayer.teamId === undefined) return;
@@ -38,12 +42,27 @@ export const GameScreen = () => {
 				);
 			});
 		},
-		[],
+		[isGameGoing, selfPlayer.id, selfPlayer.teamId],
 	);
+
+	React.useEffect(() => {
+		const id = setInterval(() => {
+			setCurrentTime(Date.now());
+		}, 1000 / 30);
+
+		return () => {
+			clearInterval(id);
+		};
+	}, []);
 
 	return (
 		<div>
 			<div>
+				{isGameGoing ? null : (
+					<div>
+						<span>{secondsTillStart}</span>
+					</div>
+				)}
 				{gameState.revealed === undefined ||
 				gameState.flags === undefined ? (
 					<div>?</div>
